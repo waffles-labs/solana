e322f300aa3f387ad8e4443e647ae3124ab7b9cd
Tao Zhu <82401714+taozhu-chicago@users.noreply.github.com>
2022-12-22 10:43:52 -0600

    Revert "fix account resize test by requesting max tx data size (#28826)"
    This reverts commit ae48ac97ddb34dbb7945d0bc5ee3449afd00e629.

--- runtime/src/bank.rs
+++ runtime/src/bank.rs
@@ -19550,10 +19550,7 @@ pub(crate) mod tests {
             account_metas,
         );
         Transaction::new_signed_with_payer(
-            &[
-                instruction,
-                ComputeBudgetInstruction::set_accounts_data_size_limit(u32::MAX),
-            ],
+            &[instruction],
             Some(&payer.pubkey()),
             &[payer],
             recent_blockhash,
@@ -19855,7 +19852,6 @@ pub(crate) mod tests {
             &mock_program_id,
             mock_realloc_process_instruction,
         );
-
         let recent_blockhash = bank.last_blockhash();
 
         let funding_keypair = Keypair::new();
@@ -19872,7 +19868,7 @@ pub(crate) mod tests {
             let account_balance = LAMPORTS_PER_SOL;
             let account_size = rng.gen_range(
                 1,
-                MAX_PERMITTED_DATA_LENGTH as usize - MAX_PERMITTED_DATA_INCREASE,
+                (MAX_PERMITTED_DATA_LENGTH / 4) as usize - MAX_PERMITTED_DATA_INCREASE,
             );
             let account_data =
                 AccountSharedData::new(account_balance, account_size, &mock_program_id);
@@ -19902,8 +19898,9 @@ pub(crate) mod tests {
         {
             let account_pubkey = Pubkey::new_unique();
             let account_balance = LAMPORTS_PER_SOL;
-            let account_size =
-                rng.gen_range(MAX_PERMITTED_DATA_LENGTH / 2, MAX_PERMITTED_DATA_LENGTH) as usize;
+            let account_size = rng
+                .gen_range(MAX_PERMITTED_DATA_LENGTH / 8, MAX_PERMITTED_DATA_LENGTH / 4)
+                as usize;
             let account_data =
                 AccountSharedData::new(account_balance, account_size, &mock_program_id);
             bank.store_account(&account_pubkey, &account_data);
