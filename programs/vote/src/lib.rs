665e92b85f49e414a6d8f5e83b8507097cd62a65
nixxholas <nixholas@icloud.com>
2023-01-09 02:11:02 +0800

    programs: vote: namespace fixes
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- programs/vote/src/lib.rs
+++ programs/vote/src/lib.rs
@@ -6,10 +6,10 @@ pub mod vote_state;
 pub mod vote_transaction;
 
 #[macro_use]
-extern crate waffles_solana_metrics;
+extern crate solana_metrics;
 
 #[macro_use]
-extern crate waffles_solana_frozen_abi_macro;
+extern crate solana_frozen_abi_macro;
 
 pub use waffles_solana_sdk::vote::{
     authorized_voters, error as vote_error, instruction as vote_instruction,
