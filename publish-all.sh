c1af8bd8a74a690cd7f14472f7905e8d4d3940eb
elvisyong <elvisyong@gmail.com>
2023-02-23 17:22:28 +0800

    build: update to 1.16.0-alpha.3

--- publish-all.sh
+++ publish-all.sh
@@ -1,148 +1,151 @@
 #cargo publish -p waffles-solana-logger
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-frozen-abi-macro
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-frozen-abi
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-sdk-macro
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-program
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-merkle-tree
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-sdk
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-measure
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-metrics
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-program-runtime
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-address-lookup-table-program
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-connection-cache
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-config-program
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-account-decoder
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-transaction-status
-#sleep 5
-#cargo publish -p waffles-solana-rpc-client-api
-#sleep 5
-#cargo publish -p waffles-solana-pubsub-client
-#sleep 5
-#cargo publish -p waffles-solana-perf
-#sleep 5
-#cargo publish -p waffles-solana-streamer
-#sleep 5
-#cargo publish -p waffles-solana-banks-interface
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-version
-#sleep 5
-#cargo publish -p waffles-solana-net-utils
-#sleep 5
+#sleep 8
+#cargo publish -p waffles-solana-rpc-client-api
+#sleep 8
+#cargo publish -p waffles-solana-pubsub-client
+#sleep 8
 #cargo publish -p waffles-solana-vote-program
-#sleep 5
+#sleep 8
+#cargo publish -p waffles-solana-perf
+#sleep 8
+#cargo publish -p waffles-solana-streamer
+#sleep 8
+#cargo publish -p waffles-solana-banks-interface
+#sleep 8
+#cargo publish -p waffles-solana-net-utils
+#sleep 8
 #cargo publish -p waffles-solana-rpc-client
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-tpu-client
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-quic-client
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-remote-wallet
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-clap-utils
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-rpc-client-nonce-utils
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-thin-client
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-udp-client
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-zk-token-sdk
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-rayon-threadlimit
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-bpf-loader-program
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-bucket-map
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-compute-budget-program
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-stake-program
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-zk-token-proof-program
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-runtime
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-client
-#sleep 5
-#cargo publish -p waffles-solana-rpc
-#sleep 5
-#cargo publish -p waffles-solana-banks-server
-#sleep 5
-#cargo publish -p waffles-solana-banks-client
-#sleep 5
-#cargo publish -p waffles-solana-bloom
-#sleep 5
-#cargo publish -p waffles-solana-clap-v3-utils
-#sleep 5
-#cargo publish -p waffles-solana-cli
-#sleep 5
-#cargo publish -p waffles-solana-cli-config
-#sleep 5
-#cargo publish -p waffles-solana-cli-output
-#sleep 5
-#cargo publish -p waffles-solana-core
-#sleep 5
-#cargo publish -p waffles-solana-download-utils
-#sleep 5
-#cargo publish -p waffles-solana-entry
-#sleep 5
-#cargo publish -p waffles-solana-faucet
-#cargo publish -p waffles-solana-genesis
-#sleep 5
-#cargo publish -p waffles-solana-genesis-utils
-#sleep 5
-#cargo publish -p waffles-solana-geyser-plugin-interface
-#sleep 5
-#cargo publish -p waffles-solana-geyser-plugin-manager
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-gossip
-#sleep 5
-#cargo publish -p waffles-solana-install
-#sleep 5
-#cargo publish -p waffles-solana-keygen
-#sleep 5
-#cargo publish -p waffles-solana-ledger
-#sleep 5
-#cargo publish -p waffles-solana-ledger-tool
-#sleep 5
-#cargo publish -p waffles-solana-local-cluster
-#sleep 5
-#cargo publish -p waffles-solana-memory-management
-#sleep 5
-#cargo publish -p waffles-solana-notifier
-#sleep 5
+#sleep 8
+#cargo publish -p waffles-solana-sys-tuner
+#sleep 8
 #cargo publish -p waffles-solana-poh
-#sleep 5
-#cargo publish -p waffles-solana-program-test
-#sleep 5
+#sleep 8
 #cargo publish -p waffles-solana-send-transaction-service
-#sleep 5
-#cargo publish -p waffles-solana-stake-accounts
-#sleep 5
-#cargo publish -p waffles-solana-storage-bigtable
-#sleep 5
+#sleep 8
+#cargo publish -p waffles-solana-rpc
+#sleep 8
+#cargo publish -p waffles-solana-banks-server
+#sleep 8
+#cargo publish -p waffles-solana-banks-client
+#sleep 8
+#cargo publish -p waffles-solana-bloom
+#sleep 8
+#cargo publish -p waffles-solana-clap-v3-utils
+#sleep 8
+#cargo publish -p waffles-solana-cli
+#sleep 8
+#cargo publish -p waffles-solana-cli-config
+#sleep 8
+#cargo publish -p waffles-solana-cli-output
+#sleep 8
+#cargo publish -p waffles-solana-entry
+#sleep 8
+#cargo publish -p waffles-solana-core
+#sleep 8
+#cargo publish -p waffles-solana-download-utils
+#sleep 8
+#cargo publish -p waffles-solana-faucet
+#sleep 8
+#cargo publish -p waffles-solana-ledger
+#sleep 8
 #cargo publish -p waffles-solana-storage-proto
-#sleep 5
-cargo publish -p waffles-solana-sys-tuner
-sleep 5
+#sleep 8
+#cargo publish -p waffles-solana-storage-bigtable
+#sleep 8
+#cargo publish -p waffles-solana-genesis
+#sleep 8
+#cargo publish -p waffles-solana-entry
+#sleep 8
+#cargo publish -p waffles-solana-genesis-utils
+#sleep 8
+#cargo publish -p waffles-solana-geyser-plugin-interface
+#sleep 8
+#cargo publish -p waffles-solana-geyser-plugin-manager
+#sleep 8
+#cargo publish -p waffles-solana-install
+#sleep 8
+#cargo publish -p waffles-solana-keygen
+#sleep 8
+#cargo publish -p waffles-solana-ledger-tool
+#sleep 8
+cargo publish -p waffles-solana-local-cluster
+sleep 8
+cargo publish -p waffles-solana-memory-management
+sleep 8
+cargo publish -p waffles-solana-notifier
+sleep 8
+cargo publish -p waffles-solana-program-test
+sleep 8
+cargo publish -p waffles-solana-stake-accounts
+sleep 8
 cargo publish -p waffles-solana-test-validator
-sleep 5
+sleep 8
 cargo publish -p waffles-solana-tokens
-sleep 5
+sleep 8
 cargo publish -p waffles-solana-validator
-sleep 5
+sleep 8
 cargo publish -p waffles-solana-watchtower
