c7fba4f621c2677616945fa2821284717b001934
elvisyong <elvisyong@gmail.com>
2023-03-24 18:29:48 +0800

    feat: update to alpha.10

--- publish-all.sh
+++ publish-all.sh
@@ -1,87 +1,87 @@
-#cargo publish -p waffles-solana-logger
-#sleep 5
-#cargo publish -p waffles-solana-frozen-abi-macro
-#sleep 5
-#cargo publish -p waffles-solana-frozen-abi
-#sleep 5
-#cargo publish -p waffles-solana-sdk-macro
-#sleep 5
-#cargo publish -p waffles-solana-program
-#sleep 5
-#cargo publish -p waffles-solana-merkle-tree
-#sleep 5
-#cargo publish -p waffles-solana-sdk
-#sleep 5
-#cargo publish -p waffles-solana-measure
-#sleep 5
-#cargo publish -p waffles-solana-metrics
-#sleep 5
-#cargo publish -p waffles-solana-rayon-threadlimit
-#sleep 5
-#cargo publish -p waffles-solana-perf
-#sleep 5
-#cargo publish -p waffles-solana-program-runtime
-#sleep 5
-#cargo publish -p waffles-solana-address-lookup-table-program
-#sleep 5
-#cargo publish -p waffles-solana-version
-#sleep 5
-#cargo publish -p waffles-solana-net-utils
-#sleep 5
-#cargo publish -p waffles-solana-connection-cache
-#sleep 5
-#cargo publish -p waffles-solana-config-program
-#sleep 5
-#cargo publish -p waffles-solana-account-decoder
-#sleep 5
-#cargo publish -p waffles-solana-transaction-status
-#sleep 5
-#cargo publish -p waffles-solana-rpc-client-api
-#sleep 5
-#cargo publish -p waffles-solana-pubsub-client
-#sleep 5
-#cargo publish -p waffles-solana-vote-program
-#sleep 5
-#cargo publish -p waffles-solana-streamer
-#sleep 5
-#cargo publish -p waffles-solana-banks-interface
-#sleep 5
-#cargo publish -p waffles-solana-rpc-client
-#sleep 5
-#cargo publish -p waffles-solana-tpu-client
-#sleep 5
-#cargo publish -p waffles-solana-quic-client
-#sleep 5
-#cargo publish -p waffles-solana-remote-wallet
-#sleep 5
-#cargo publish -p waffles-solana-clap-utils
-#sleep 5
-#cargo publish -p waffles-solana-rpc-client-nonce-utils
-#sleep 5
-#cargo publish -p waffles-solana-thin-client
-#sleep 5
-#cargo publish -p waffles-solana-udp-client
-#sleep 5
-#cargo publish -p waffles-solana-zk-token-sdk
-#sleep 5
-#cargo publish -p waffles-solana-bpf-loader-program
-#sleep 5
-#cargo publish -p waffles-solana-bucket-map
-#sleep 5
-#cargo publish -p waffles-solana-compute-budget-program
-#sleep 5
-#cargo publish -p waffles-solana-stake-program
-#sleep 5
-#cargo publish -p waffles-solana-zk-token-proof-program
-#sleep 5
-#cargo publish -p waffles-solana-runtime
-#sleep 5
-#cargo publish -p waffles-solana-client
-#sleep 5
-#cargo publish -p waffles-solana-storage-proto
-#sleep 5
-#cargo publish -p waffles-solana-storage-bigtable
-#sleep 5
+cargo publish -p waffles-solana-logger
+sleep 5
+cargo publish -p waffles-solana-frozen-abi-macro
+sleep 5
+cargo publish -p waffles-solana-frozen-abi
+sleep 5
+cargo publish -p waffles-solana-sdk-macro
+sleep 5
+cargo publish -p waffles-solana-program
+sleep 5
+cargo publish -p waffles-solana-merkle-tree
+sleep 5
+cargo publish -p waffles-solana-sdk
+sleep 5
+cargo publish -p waffles-solana-measure
+sleep 5
+cargo publish -p waffles-solana-metrics
+sleep 5
+cargo publish -p waffles-solana-rayon-threadlimit
+sleep 5
+cargo publish -p waffles-solana-perf
+sleep 5
+cargo publish -p waffles-solana-program-runtime
+sleep 5
+cargo publish -p waffles-solana-address-lookup-table-program
+sleep 5
+cargo publish -p waffles-solana-version
+sleep 5
+cargo publish -p waffles-solana-net-utils
+sleep 5
+cargo publish -p waffles-solana-connection-cache
+sleep 5
+cargo publish -p waffles-solana-config-program
+sleep 5
+cargo publish -p waffles-solana-account-decoder
+sleep 5
+cargo publish -p waffles-solana-transaction-status
+sleep 5
+cargo publish -p waffles-solana-rpc-client-api
+sleep 5
+cargo publish -p waffles-solana-pubsub-client
+sleep 5
+cargo publish -p waffles-solana-vote-program
+sleep 5
+cargo publish -p waffles-solana-streamer
+sleep 5
+cargo publish -p waffles-solana-banks-interface
+sleep 5
+cargo publish -p waffles-solana-rpc-client
+sleep 5
+cargo publish -p waffles-solana-tpu-client
+sleep 5
+cargo publish -p waffles-solana-quic-client
+sleep 5
+cargo publish -p waffles-solana-remote-wallet
+sleep 5
+cargo publish -p waffles-solana-clap-utils
+sleep 5
+cargo publish -p waffles-solana-rpc-client-nonce-utils
+sleep 5
+cargo publish -p waffles-solana-thin-client
+sleep 5
+cargo publish -p waffles-solana-udp-client
+sleep 5
+cargo publish -p waffles-solana-zk-token-sdk
+sleep 5
+cargo publish -p waffles-solana-bpf-loader-program
+sleep 5
+cargo publish -p waffles-solana-bucket-map
+sleep 5
+cargo publish -p waffles-solana-compute-budget-program
+sleep 5
+cargo publish -p waffles-solana-stake-program
+sleep 5
+cargo publish -p waffles-solana-zk-token-proof-program
+sleep 5
+cargo publish -p waffles-solana-runtime
+sleep 5
+cargo publish -p waffles-solana-client
+sleep 5
+cargo publish -p waffles-solana-storage-proto
+sleep 5
+cargo publish -p waffles-solana-storage-bigtable
+sleep 5
 cargo publish -p waffles-solana-entry
 sleep 5
 cargo publish -p waffles-solana-ledger
